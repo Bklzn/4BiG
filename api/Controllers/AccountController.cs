b749e4a0f5f152162565c1899fb9a731d2ff883f
Qajgor <boradyn555@gmail.com>
2022-05-30 18:29:05 +0200

    Coś się z api zepsuło

---
+++ api/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using _4bigData.Models;
+using _4big.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4big.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+            _accountService.RegisterUser(dto);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] LoginDto dto)
+        {
+            string token = _accountService.GenerateJwt(dto);
+            return Ok(token);
+        }
+    }
+}
---
+++ api/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4big.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
