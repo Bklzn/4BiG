d97181a4e3ed6a14de4a369146e166fc0a8c2fa7
Qajgor <boradyn555@gmail.com>
2022-05-30 15:51:45 +0200

    Funkcje łączące się z api

---
+++ apiConnection/Models/Order.js
@@ -0,0 +1,16 @@
+class Order{
+    constructor(Email, PhoneNumber, FirstName, LastName, StreetName, BuildingNum, ApartmentNum, ZipCode, City, Nip, PickupForm, CookiesIds){
+        this.Email = Email
+        this.PhoneNumber = PhoneNumber
+        this.FirstName = FirstName
+        this.LastName = LastName
+        this.StreetName = StreetName
+        this.BuildingNum = BuildingNum
+        this.ApartmentNum = ApartmentNum
+        this.ZipCode = ZipCode
+        this.City = City
+        this.Nip = Nip
+        this.PickupForm = PickupForm
+        this.CookiesIds = CookiesIds
+    }
+}
---
+++ apiConnection/Models/UserReg.js
@@ -0,0 +1,8 @@
+class UserReg{
+    constructor(Email, Password, PasswordConfirmed, Nickname){
+        this.Email = Email
+        this.Password = Password
+        this.PasswordConfirmed = PasswordConfirmed
+        this.Nickname = Nickname
+    }
+}
